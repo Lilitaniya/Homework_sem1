ad3834d4b43314e27f678a18ff9a708196b8e203
Bazhena Filippovskaia <kagome-fortuna@ya.ru>
2023-09-22 22:09:37 +0300

    Сделана домашка 6

---
+++ hw6/ex.41/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3 */
+
+Console.Write("Введите числа, разделяя их запятой: ");
+string input = Console.ReadLine();
+string[] numberStrings = input.Split(','); 
+int count = 0; 
+    foreach (string numberStr in numberStrings)
+    {
+        if (double.TryParse(numberStr, out double number) && number > 0)
+        {
+            count++;
+        }
+    }
+Console.WriteLine($"Из введенных чисел больше 0: {count}");
