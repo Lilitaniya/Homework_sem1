dfaad0e91466ead26386f76c6d7f8402338719c2
Bazhena Filippovskaia <kagome-fortuna@ya.ru>
2023-10-06 23:32:02 +0300

    Сделана домашка 9 (итоговая)

---
+++ hw9/ex.64/Program.cs
@@ -0,0 +1,37 @@
+﻿/* Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1" */
+
+class Program
+{
+    static void Main()
+    {
+        int N = 5; 
+
+        Console.Write("Натуральные числа от " + N + " до 1 с помощью рекурсии: ");
+        PrintNaturalNumbers(N);
+
+        Console.Write("."); 
+
+        Console.ReadKey();
+    }
+
+    static void PrintNaturalNumbers(int N)
+    {
+        if (N < 1)
+        {
+            return;
+        }
+
+        if (N == 1)
+        {
+            Console.Write(N); 
+        }
+        else
+        {
+            Console.Write(N + ", "); 
+        }
+
+        PrintNaturalNumbers(N - 1);
+    }
+}
---
+++ hw9/ex.66/Program.cs
@@ -0,0 +1,29 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+class Program
+{
+    static void Main()
+    {
+
+        int M = 1;
+        int N = 15;
+
+        int sum = SumOfNaturalNumbers(M, N);
+
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от {M} до {N} = {sum}");
+    }
+
+    static int SumOfNaturalNumbers(int M, int N)
+    {
+        if (M == N)
+        {
+            return M;
+        }
+        else
+        {
+            return M + SumOfNaturalNumbers(M + 1, N);
+        }
+    }
+}
